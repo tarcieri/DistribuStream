2adfb1a648c76b8be2bdacf1ec1f8d4a13a68c13
Galen Pahlke <pahlke@gmail.com>
2006-10-13 00:57:46 +0000

    added some test stuff

---
+++ scratch/experiment1/netutil.rb
@@ -0,0 +1,57 @@
+require "stringio"
+
+class MarshalBuffer
+	def appendString(str)
+		@buffer||=String.new
+		@buffer=@buffer+str	
+	end
+
+	def readObject
+		@buffer||=String.new
+		io=StringIO.new(@buffer)
+		obj=Marshal.load(io) rescue nil
+		@buffer=@buffer[io.tell,@buffer.length-1] if obj
+		return obj	
+	end
+end
+
+module Connection
+
+	def send(object) 
+	end
+	
+	#returns the next object from this connection or nil if nothing is available
+	def recv 
+	end
+
+end
+
+
+module MarshalEMConnection 
+	
+	def post_init
+		@mbuffer=MarshalBuffer.new
+	end
+
+	def receive_data data
+		@mbuffer||=MarshalBuffer.new
+		@mbuffer.appendString data
+		@connectionListener.connectionEvent(self)
+	end
+	
+	def recv
+		@mbuffer||=MarshalBuffer.new
+		return @mbuffer.readObject
+	end
+	
+	def send(obj)
+		send_data(Marshal.dump(obj))
+	end
+
+end
+
+module ClientMarshalEMConnection
+	include MarshalEMConnection
+	
+end
+
---
+++ scratch/experiment1/server.rb
@@ -0,0 +1,25 @@
+require "netutil"
+require "eventmachine"
+
+class Server
+
+	def connectionEvent(con)
+		puts "got connection event"
+	end
+
+end
+
+$server=Server.new
+
+module ServerMarshalEMConnection
+	include MarshalEMConnection
+	
+	def post_init
+		@connectionListener=$server
+	end
+end
+
+
+EventMachine::run do
+	EventMachine::start_server "127.0.0.1", 8081, ServerMarshalEMConnection
+end
