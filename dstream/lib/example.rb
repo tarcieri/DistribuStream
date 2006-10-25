f92125ec52b89dd475fc03b3507ec8c03088a51e
Tony Arcieri <tony@medioh.com>
2006-10-25 01:25:02 +0000

    Add 'rake spec' support to Rakefile and example library with RDoc and RSpec instrumentation

---
+++ dstream/lib/example.rb
@@ -0,0 +1,17 @@
+#--
+# Copyright 2006 by ClickCaster, Inc. (info@clickcaster.com)
+# All rights reserved.  See LICENSE.txt for permissions.
+# 
+# This source file is distributed as part of the 
+# DistribuStream file transfer system.
+#
+# See http://pdtp.org/
+#++
+
+# An example class.  Here's where you'd describe it
+class Example
+    # An example instance method.  Description for RDoc goes here
+    def example
+        'Example'
+    end
+end
