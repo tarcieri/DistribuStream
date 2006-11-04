3dd91d9a6acde2ccdcead61c1cbdcff9fd44406a
Galen Pahlke <pahlke@gmail.com>
2006-11-04 21:44:17 +0000

    testing rdoc

---
+++ dstream/lib/server.rb
@@ -0,0 +1,88 @@
+require File.dirname(__FILE__)+ '/message'
+#require File.dirname(__FILE__)+ '/filemanager'
+
+class ClientData
+	def initialize(address)
+		@trusts={}
+    @address=address
+		#@chunks_provided=ChunksProvided.new
+	end
+
+	attr_accessor :rank #global peer rank of this client
+	attr_accessor :trusts #list of trust relationships between other clients
+	attr_accessor :chunks_provided
+	attr_accessor :address
+end
+
+
+
+
+class Server < MessageClient
+	attr_accessor :file_manager
+	
+	def initialize
+		@clients={}
+	end	
+
+  #sends out a transfer packet to the connector
+  # +connector+ address of client that connects
+  # +listener+ address of listener (may be client or server)
+  # +mode+ either :give or :take.  determines the direction of transfer (relative to the connector)
+  # +path+ location of file to transfer
+  # +chunkid+ integer number of the chunk to transfer 
+  def start_transfer(connector,listener,mode,path,chunkid)
+  
+  end
+
+	def dispatch(message)
+
+    
+		handlers={ 
+			RequestPacket=> :dispatch_packet
+		}
+
+		handler=handlers[ message.class ]
+		return false if handler.nil?
+		
+		method(handler).call(message)
+    return true	
+	end
+  
+  def dispatch_packet(packet)
+    #add a client if they just connected
+    client=@clients[packet.source]||=ClientData.new(packet.source)
+    
+    handlers={
+      :ask_info=> :dispatch_askinfo
+    }
+  
+    handler=handlers[ packet.data[:type] ]
+    method(handler).call(client,packet.data)
+
+  end
+
+	
+	def dispatch_askinfo(client,data)
+    ret= {
+      :type => :tell_info,
+      :path => data[:path]
+    }
+    post(ResponsePacket.new(client.address,ret))
+  end  
+
+=begin
+		ret= {
+			:type => :tellinfo,
+			:filename => packet[:filename]
+		}
+		info=file_manager.get_info( packet[:filename])
+		if info==nil 
+			ret[:status]=:notfound
+		else
+			ret[:size]=info.size
+			ret[:status]=:normal
+		end
+=end
+	
+
+end
