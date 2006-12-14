e8e3093bdb50dbdf30ec2a151f31a5fcd7b55485
Galen Pahlke <pahlke@gmail.com>
2006-12-14 22:33:00 +0000

    started making network simulator

--- dstream/simulation/network_simulator.rb
+++ dstream/simulation/network_simulator.rb
@@ -1,12 +1,105 @@
-require File.dirname(__FILE__) + '/../lib/common/message'
 
-# handles ResponsePackets, produces RequestPackets
-# the addresses used in the network simulator are simply references to the
-# MessageManagers of the nodes on the network
-class NetworkSimulator < MessageClient
+class Address
+  def initialize(address,port)
+    @address,@port=address,port
+  end
+  attr_accessor :address,:port
+end
 
-  def dispatch_from(message,from)
-    return false if message.class != PacketOut
-    message.dest.post(PacketIn.new(from,message.data) )
+class MessageQueueItem
+  def initialize(connection,message)
+    @connection,@message=connection,message
+  end
+  attr_accessor :connection,:message
+end
+
+#this class implements the NetworkManager interface well enough for it to be used in testing
+class NetworkManagerSim
+
+  class Connection
+    attr_accessor :local,:remote #references to the local and remote NetworkManagerSim
+
+    def send_message(message)
+      remote.connection.incoming_message(self,message)
+    end
+
+  end
+
+  attr_accessor :connections,:listeners #needed so we can access another NetworkManagerSim's connections
+  protected :connections,:listeners
+
+  @@message_queue=Array.new
+
+  def initialize
+    @listeners=Array.new
+    @connections=Array.new
+    @unique_source_port=5000
+  end
+
+  def connect(address)
+    @unique_source_port=@unique_source_port+1
+
+    con=Connection.new
+    con.local=[self,@unique_source_port]
+    con.remote=address
+    @connections<<con
+
+    #open a connection back from the remote host
+    con2=Connection.new
+    con2.local=con.remote
+    con2.remote=con.local
+    address[0].connections<<con
+    
+    return con
+  end
+
+  #source connection is the connection that sent this message from the other side
+  def incoming_message(source_connection,message)
+    #puts "source_connection=#{source_connection.inspect}"
+    #puts "@connections=#{@connections.inspect}"
+    #cons=@connections.select {|c| c.local==source_connection.remote && c.remote==source_connection.local }
+    #connection=cons[0] rescue nil
+    connection=@connections[0]
+    @@message_queue<<[connection,message] 	
+  end
+
+  def register_message_listener(listener)
+    @listeners<<listener
+  end
+
+  def NetworkManagerSim.run
+
+    while @@message_queue.size>0 do
+      puts "mq=#{@@message_queue.inspect}"
+      message=@@message_queue.pop
+      
+      net_sim=message[0].local
+      net_sim.listeners.each { |listener| listener.dispatch_message(message[1],message[0]) }  
+    end 
+
+  end
+
+end
+
+class BasicMessageHandler 
+  def dispatch_message(message,connection)
+    puts "dispatching message: #{message.inspect} from connection: #{connection.inspect}"
   end
 end
+
+c1net=NetworkManagerSim.new
+c2net=NetworkManagerSim.new
+
+c1=BasicMessageHandler.new
+c2=BasicMessageHandler.new
+
+c1net.register_message_listener(c1)
+c2net.register_message_listener(c2)
+
+
+connection=c1net.connect([c2net,5000])
+connection.send_message("This is a message.")
+NetworkManagerSim.run
+
+
+
