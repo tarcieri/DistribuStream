c40b70492e48023d5bee809681c8dc9cdaff8371
Galen Pahlke <pahlke@gmail.com>
2007-01-29 03:24:33 +0000

    added message translator and client info

---
+++ dstream/lib/server/client_info.rb
@@ -0,0 +1,42 @@
+class ClientInfo
+  attr_accessor :chunk_info
+ 
+end
+
+class ChunkInfo
+	def initialize
+		@files={}
+	end
+
+  #each chunk can either be provided, requested, transfer, or none
+
+  def provide(filename,range); set(filename,range,:provided) ; end
+  def unprovide(filename,range); set(filename,range, :none); end
+  def request(filename,range); set(filename,range, :requested); end
+  def unrequest(filename,range); set(filename,range, :none); end
+  def transfer(filename,range); set(filename,range, :transfer); end
+  
+  def provided?(filename,chunk); get(filename,chunk) == :provided; end
+	def requested?(filename,chunk); get(filename,chunk) == :requested; end
+ 
+  #returns a high priority requested chunk
+  def high_priority_chunk
+    #right now return any chunk
+    @files.each do |name,file|
+      file.each_index do |i|
+        return [name,i] if file[i]==:requested
+      end
+    end  
+    return nil
+  end
+    
+protected
+  def get(filename,chunk)
+    return @files[filename][chunk] rescue :neither
+  end
+
+  def set(filename,range,state)
+    chunks=@files[filename]||=Array.new
+    range.each { |i| chunks[i]=state }
+  end
+end
---
+++ dstream/lib/server/server_message_translator.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__)+'/../common/pdtp_protocol'
+
+class ServerMessageTranslator < PDTPProtocol
+  def initialize *args
+    super
+  end
+
+  attr_accessor :user_info # users of this class may store user specific data here
+
+
+  @@server=nil #reference to a server object to communicate with
+
+  def ServerMessageTranslator::server= server
+    @@server=server
+  end
+
+  def receive_message message
+    begin
+      case message["type"]
+      when "ask_info"
+        @@server.ask_info(self,message["url"])
+      else
+        raise
+      end
+    rescue 
+      close_connection # protocol error
+    end
+  end
+ 
+end
