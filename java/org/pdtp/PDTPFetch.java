241d7a63af7422f4178f387a94a90fe317c2641a
Ashvin Mysore <black.iris.dancer@gmail.com>
2007-02-18 20:49:08 +0000

    Better naming.

--- java/org/pdtp/PDTPFetch.java
+++ java/org/pdtp/PDTPFetch.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
-public class Tester {
+public class PDTPFetch {
   public static void main(String[] args) throws IOException {    
     Network N = new Network("catclops.clickcaster.com", 6000, new MemoryCache());
     ReadableByteChannel c = N.get("http://www.feministe.us/", 1000);
